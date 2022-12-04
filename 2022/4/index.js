8c42e2df11f6f4f075b3311aef0e830200f73a15
Niklas <niklas.frank@tretton37.com>
2022-12-04 15:33:04 +0100

    refactor: numerical folders

---
+++ 2022/4/index.js
@@ -0,0 +1,21 @@
+/**
+ *
+ * @param {*} inputs
+ */
+export const partOne = (inputs) => {};
+/**
+ *
+ * @param {*} inputs
+ */
+export const partTwo = (inputs) => {};
+
+
+(async () => {
+  const fs = await import("node:fs");
+  const path = await import("node:path");
+  const input = fs.readFileSync(
+    path.resolve(__dirname, "./input.txt"),
+    "utf-8"
+  );
+  console.log(partOne(input), partTwo(input));
+})();
---
+++ 2022/4/index.test.js
@@ -0,0 +1,12 @@
+import { expect, test } from "vitest";
+import { partOne, partTwo } from ".";
+
+const data = ``;
+
+test.skip("partOne works", () => {
+  expect(partOne(data)).toEqual(0);
+});
+
+test.skip("partTwo works", () => {
+  expect(partTwo(data)).toEqual(0);
+});
