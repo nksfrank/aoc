3f4a6a03bb8137e0695acd63abcdf64a3abd3d3d
Niklas <niklas.frank@tretton37.com>
2022-12-06 20:40:58 +0100

    day 4

--- 2022/4/index.js
+++ 2022/4/index.js
@@ -1,14 +1,27 @@
+const parseInputs = (inputs) =>
+  inputs
+    .split("\n")
+    .map((pair) =>
+      pair
+        .split(",")
+        .map((elf) => elf.split("-").map((n) => Number.parseInt(n)))
+    );
 /**
  *
  * @param {*} inputs
  */
-export const partOne = (inputs) => {};
+export const partOne = (inputs) =>
+  parseInputs(inputs).filter(
+    ([[a0, a1], [b0, b1]]) => (a0 <= b0 && a1 >= b1) || (b0 <= a0 && b1 >= a1)
+  ).length;
 /**
  *
  * @param {*} inputs
  */
-export const partTwo = (inputs) => {};
-
+export const partTwo = (inputs) =>
+  parseInputs(inputs).filter(
+    ([[a0, a1], [b0, b1]]) => (a0 >= b0 && a0 <= b1) || (b0 >= a0 && b0 <= a1)
+  ).length;
 
 (async () => {
   const fs = await import("node:fs");
--- 2022/4/index.test.js
+++ 2022/4/index.test.js
@@ -1,12 +1,27 @@
 import { expect, test } from "vitest";
 import { partOne, partTwo } from ".";
 
-const data = ``;
+const data = `2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8`;
 
-test.skip("partOne works", () => {
-  expect(partOne(data)).toEqual(0);
+test.each([
+  [data, 2],
+  [
+    `2-6,2-6
+  2-6,6-7
+  2-6,4-6
+  2-2,2-6
+  2-6,6-6`,
+    4,
+  ],
+])("partOne works", (input, expected) => {
+  expect(partOne(input)).toEqual(expected);
 });
 
-test.skip("partTwo works", () => {
-  expect(partTwo(data)).toEqual(0);
+test("partTwo works", () => {
+  expect(partTwo(data)).toEqual(4);
 });
