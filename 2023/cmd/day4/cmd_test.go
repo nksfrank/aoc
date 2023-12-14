404ceef5198ae558036714f5b7ccfe4680f4e80d
Niklas <niklas.frank@tretton37.com>
2023-12-14 07:26:38 +0100

    add test for each card

--- 2023/cmd/day4/cmd_test.go
+++ 2023/cmd/day4/cmd_test.go
@@ -2,19 +2,57 @@ package day4
 
 import (
 	"io"
+	"strings"
 	"testing"
 
 	"github.com/nksfrank/aoc/cmd/cmdtest"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestDayTwo(t *testing.T) {
+func TestDayFour(t *testing.T) {
 	tests := []struct {
 		name string
 		want int
 		input io.Reader
 		fn func(r io.Reader) int
 	}{
+		{
+			name: "card 1",
+			want: 8,
+			input: strings.NewReader("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"),
+			fn: partOne,
+		},
+		{
+			name: "card 2",
+			want: 2,
+			input: strings.NewReader("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19"),
+			fn: partOne,
+		},
+		{
+			name: "card 3",
+			want: 2,
+			input: strings.NewReader("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1"),
+			fn: partOne,
+		},
+		{
+			name: "card 4",
+			want: 1,
+			input: strings.NewReader("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83"),
+			fn: partOne,
+		},
+		{
+			name: "card 5",
+			want: 0,
+			input: strings.NewReader("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36"),
+			fn: partOne,
+		},
+		{
+			name: "card 6",
+			want: 0,
+			input: strings.NewReader("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"),
+			fn: partOne,
+		},
+
 		{
 			name: "part one",
 			want: 13,
