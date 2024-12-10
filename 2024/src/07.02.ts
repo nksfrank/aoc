78229d7168fddc55fc80f5e27b2e830823a12db2
nksfrank <niklas.frank@gmail.com>
2024-12-10 08:53:37 +0100

    bug: fix segment fault

--- 2024/src/07.02.ts
+++ 2024/src/07.02.ts
@@ -1,6 +1,5 @@
 import "../pkg";
-import { run } from "../utils";
-import data from "./07.input.txt" with { type: "file" };
+import { assert } from "../pkg";
 
 export function partTwo(input: string): number {
   const data = input
@@ -8,15 +7,13 @@ export function partTwo(input: string): number {
     .map((l) => l.split(":"))
     .map(([test, values]) => [Number(test), values.trim().split(" ")] as const);
 
-  const rerun: [number, string[]][] = [];
+  const rerun: [number, readonly string[]][] = [];
   let res = 0;
-  let idx = data.length;
   for (const [test, values] of data) {
     let found = false;
     for (const ops of operations(values.length - 1, ["+", "*"])) {
       if (test === compute(values, ops)) {
         res += test;
-        console.log("leftover", --idx);
         found = true;
         break;
       }
@@ -24,31 +21,31 @@ export function partTwo(input: string): number {
     if (found) continue;
     rerun.push([test, values]);
   }
-  console.log("concatenating", rerun.length, "tests");
-  let rerunRes = 0;
-  idx = rerun.length;
   for (const [test, values] of rerun) {
     for (const ops of operations(values.length - 1, ["+", "*", "||"])) {
       if (test === compute(values, ops)) {
-        rerunRes += test;
-        console.log("leftover", --idx);
+        res += test;
         break;
       }
     }
   }
 
-  console.log(res, rerunRes, res + rerunRes);
-  return res + rerunRes;
+  return res;
 }
 
-function compute(values: string[], ops: string[]): number {
+function compute(values: readonly string[], ops: string[]): number {
+  if (values.length === 0) return 0;
+  assert(ops.length === values.length - 1, "invalid ops length");
+
   let res = values[0];
   for (let i = 0; i < ops.length; i++) {
+    assert(!!values[i + 1], "missing value");
+
     if (ops[i] === "||") {
-      res = eval(`${res}${values[i + 1]}`);
-      continue;
+      res = String(res) + String(values[i + 1]);
+    } else {
+      res = eval(res + ops[i] + values[i + 1]);
     }
-    res = eval(res + ops[i] + values[i + 1]);
   }
   return Number(res);
 }
@@ -58,16 +55,11 @@ function* operations(
 ): Generator<string[]> {
   if (n === 0) {
     yield [];
-  } else {
-    for (const op of operators) {
-      for (const ops of operations(n - 1, operators)) {
-        yield [op, ...ops];
-      }
+    return;
+  }
+  for (const op of operators) {
+    for (const ops of operations(n - 1, operators)) {
+      yield [op, ...ops];
     }
   }
 }
-
-run(async () => {
-  const input = await Bun.file(data).text();
-  console.log("Day 7 part 2:", partTwo(input));
-});
