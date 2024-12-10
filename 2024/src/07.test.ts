89ab296025e3c3157f69832458f6f3708aaa2ead
nksfrank <niklas.frank@gmail.com>
2024-12-10 15:34:21 +0100

    refactor: remove eval usage

--- 2024/src/07.test.ts
+++ 2024/src/07.test.ts
@@ -1,7 +1,13 @@
 import { describe, test, expect } from "bun:test";
-import { partTwo } from "./07.02";
+import { partOne, partTwo } from "./07";
 
 describe("07.02", () => {
+  test("partOne ", async () => {
+    const input = await Bun.file(
+      new URL("07.test.txt", import.meta.url)
+    ).text();
+    expect(partOne(input)).toBe(3749);
+  });
   test("partTwo", async () => {
     const input = await Bun.file(
       new URL("07.test.txt", import.meta.url)
--- 2024/src/07.ts
+++ 2024/src/07.ts
@@ -1,6 +1,94 @@
+import "../pkg";
+import { assert } from "../pkg";
 import { run } from "../utils";
-import { partOne } from "./07.01";
-import { partTwo } from "./07.02";
+
+function findTarget(
+  target: number,
+  values: readonly number[],
+  operators: Operators[]
+): boolean {
+  if (values.length === 0) return false;
+  for (const ops of operations(values.length - 1, operators)) {
+    if (target === compute(values, ops)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+const opsfn = {
+  "+": (a: number, b: number) => a + b,
+  "*": (a: number, b: number) => a * b,
+  "||": (a: number, b: number) => Number(String(a) + String(b)),
+} as const;
+
+function compute(values: readonly number[], ops: Operators[]): number {
+  if (values.length === 0) return 0;
+  assert(ops.length === values.length - 1, "invalid ops length");
+
+  let res = values[0];
+  for (let i = 0; i < ops.length; i++) {
+    assert(!!values[i + 1], "missing value");
+    res = opsfn[ops[i]](res, values[i + 1]);
+  }
+  return Number(res);
+}
+
+type Operators = "+" | "*" | "||";
+function* operations(
+  n: number,
+  operators: Operators[]
+): Generator<Operators[]> {
+  if (n === 0) {
+    yield [];
+    return;
+  }
+  for (const op of operators) {
+    for (const ops of operations(n - 1, operators)) {
+      yield [op, ...ops];
+    }
+  }
+}
+
+export function partOne(input: string): number {
+  const data = input
+    .split("\n")
+    .map((l) => l.split(":"))
+    .map(
+      ([test, values]) =>
+        [Number(test), values.trim().split(" ").map(Number)] as const
+    );
+
+  let res = 0;
+  for (const [target, values] of data) {
+    const found = findTarget(target, values, ["+", "*"]);
+    if (found) {
+      res += target;
+    }
+  }
+
+  return res;
+}
+
+export function partTwo(input: string): number {
+  const data = input
+    .split("\n")
+    .map((l) => l.split(":"))
+    .map(
+      ([test, values]) =>
+        [Number(test), values.trim().split(" ").map(Number)] as const
+    );
+
+  let res = 0;
+  for (const [target, values] of data) {
+    const found = findTarget(target, values, ["+", "*", "||"]);
+    if (found) {
+      res += target;
+    }
+  }
+
+  return res;
+}
 
 run(async () => {
   const input = await Bun.file(new URL("07.input.txt", import.meta.url)).text();
