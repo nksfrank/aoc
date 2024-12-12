16ef75cff5adff397e926810f0070e1bf2569c3d
nksfrank <niklas.frank@gmail.com>
2024-12-12 06:45:07 +0100

    feat: improve even number of digits conversion

--- 2024/src/11.ts
+++ 2024/src/11.ts
@@ -1,31 +1,19 @@
 import { run } from "../utils";
-const memoCache = new Map<number, number[]>();
 
 const rules = (stone: number): number[] => {
-  // Check if result is in cache
-  if (memoCache.has(stone)) {
-    return memoCache.get(stone)!;
-  }
-
-  let result: number[];
   if (stone === 0) {
-    result = [1];
-  } else {
-    const n = String(stone);
-    if (n.length % 2 === 0) {
-      result = [
-        Number(n.substring(0, n.length / 2)),
-        Number(n.substring(n.length / 2)),
-      ];
-    } else {
-      result = [stone * 2024];
-    }
+    return [1];
   }
-
-  // Store result in cache
-  memoCache.set(stone, result);
-  return result;
+  const n = (Math.log(stone) * Math.LOG10E + 1) | 0;
+  if (n % 2 === 0) {
+    return [
+      Math.floor(stone / Math.pow(10, n / 2)),
+      Math.floor(stone % Math.pow(10, n / 2)),
+    ];
+  }
+  return [stone * 2024];
 };
+
 export function partOne(input: string): number {
   let stones = input.split(" ").map((s) => Number(s));
   let blinks = 25;
