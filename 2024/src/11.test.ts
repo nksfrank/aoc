010bd76c58c9e41b883f00fb7279b92b263714df
nksfrank <niklas.frank@gmail.com>
2024-12-15 13:38:05 +0100

    feat: complete day 11 part 2

--- 2024/src/11.test.ts
+++ 2024/src/11.test.ts
@@ -1,6 +1,12 @@
-import { test, expect } from "bun:test";
-import { partOne } from "./11";
+import { test, expect, describe } from "bun:test";
+import { partOne, partTwo } from "./11";
 
-test("partOne", () => {
-  expect(partOne("125 17")).toBe(55312);
+describe("11", () => {
+  test("partOne", () => {
+    expect(partOne("125 17", 25)).toBe(55312);
+  });
+
+  test("partTwo", () => {
+    expect(partTwo("125 17", 25)).toBe(55312);
+  });
 });
--- 2024/src/11.ts
+++ 2024/src/11.ts
@@ -14,17 +14,40 @@ const rules = (stone: number): number[] => {
   return [stone * 2024];
 };
 
-export function partOne(input: string): number {
+export function partOne(input: string, blinks: number): number {
   let stones = input.split(" ").map((s) => Number(s));
-  let blinks = 25;
   while (blinks--) {
     stones = stones.flatMap(rules);
   }
-  console.log(stones);
   return stones.length;
+}
+
+export function partTwo(input: string, blinks: number): number {
+  const cache = input
+    .split(" ")
+    .map((s) => Number(s))
+    .reduce((cache, num) => {
+      const count = cache.get(num) ?? 0;
+      cache.set(num, count + 1);
+      return cache;
+    }, new Map<number, number>());
+
+  const res = Array.from({ length: blinks }).reduce(
+    (cache: Map<number, number>) =>
+      Array.from(cache.entries()).reduce((new_cache, [num, count]) => {
+        rules(num).forEach((num) => {
+          const new_count = new_cache.get(num) ?? 0;
+          new_cache.set(num, new_count + count);
+        });
+        return new_cache;
+      }, new Map<number, number>()),
+    cache
+  );
+  return Array.from(res.values()).reduce((sum, num) => sum + num, 0);
 }
 
 run(async () => {
   const input = await Bun.file(new URL("11.input.txt", import.meta.url)).text();
-  console.log("Day 11 part 1:", partOne(input));
+  console.log("Day 11 part 1:", partOne(input, 25));
+  console.log("Day 11 part 2:", partTwo(input, 75));
 });
