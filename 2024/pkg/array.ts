7ae050ba08776ec721159f0fd1a7650819630905
nksfrank <niklas.frank@gmail.com>
2024-12-16 08:39:49 +0100

    feat: add Array.range

--- 2024/pkg/array.ts
+++ 2024/pkg/array.ts
@@ -2,14 +2,27 @@ declare global {
   interface Array<T> {
     splitByElement(this: Array<T>, elem: T): [T[], T[]];
   }
+  interface ArrayConstructor {
+    range(start: number, end: number): number[];
+  }
 }
-Array.prototype.splitByElement = function <T>(
-  this: Array<T>,
-  elem: T
-): [T[], T[]] {
-  const i = this.findIndex((e) => e === elem);
-  return [this.slice(0, i), this.slice(i + 1)];
-};
+
+Object.defineProperties(Array.prototype, {
+  splitByElement: {
+    value: function <T>(this: Array<T>, elem: T): [T[], T[]] {
+      const i = this.findIndex((e) => e === elem);
+      return [this.slice(0, i), this.slice(i + 1)];
+    },
+  },
+});
+
+Object.defineProperties(Array, {
+  range: {
+    value: function (start: number, end: number): number[] {
+      return Array.from({ length: end - start }, (_, i) => i + start);
+    },
+  },
+});
 
 export function zip<T>(...arrays: T[][]): [...T[]][] {
   const minLen = Math.min(...arrays.map((arr) => arr.length));
